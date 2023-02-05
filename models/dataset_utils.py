ff7de36f73d4d88140c2a69303d9a9b78d6d485d
TM_MM_Windows_PC <tayyab.mujahid@gmail.com>
2023-02-05 18:07:45 +0300

    iam data loading in progress

--- models/dataset_utils.py
+++ models/dataset_utils.py
@@ -10,41 +10,41 @@ from xml.etree import ElementTree as ET
 logging.basicConfig(level=logging.DEBUG)
 
 
-# class BaseDataset(Dataset):
-#     def __init__(self, root_dir, transform=None):
-#         super(BaseDataset, self).__init__()
-#         self.root_dir = root_dir
-#         self.transform = transform
-#         self.img_paths = self._get_image_paths()
+class BaseDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        super(BaseDataset, self).__init__()
+        self.root_dir = root_dir
+        self.transform = transform
+        self.img_paths = self._get_image_paths()
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, idx):
+        img_name = self.img_paths[idx]
+        image = Image.open(img_name)
+        if self.transform:
+            image = self.transform(image)
+        return image, 'na'
+
+    def _get_image_paths(self):
+        return [os.path.join(self.root_dir, i)
+                for i in os.listdir(self.root_dir)]
+
+    def get_random_samples(self, number=9):
+        random_imgs = random.sample(self.img_paths, number)
+        random_imgs = [Image.open(i) for i in random_imgs]
+        return random_imgs
+
 #
-#     def __len__(self):
-#         return len(self.img_paths)
-#
-#     def __getitem__(self, idx):
-#         img_name = self.img_paths[idx]
-#         image = Image.open(img_name)
-#         if self.transform:
-#             image = self.transform(image)
-#         return image, 'na'
-#
-#     def _get_image_paths(self):
-#         return [os.path.join(self.root_dir, i)
-#                 for i in os.listdir(self.root_dir)]
-#
-#     def get_random_samples(self, number=9):
-#         random_imgs = random.sample(self.img_paths, number)
-#         random_imgs = [Image.open(i) for i in random_imgs]
-#         return random_imgs
-#
-#
-# class IAMDataset(BaseDataset):
-#     """
-#     train_root_dir = '/home/mujahid/PycharmProjects/ssl_wordspotting/datasets/IEHR/words_training'
-#     test_root_dir = '/home/mujahid/PycharmProjects/ssl_wordspotting/datasets/IEHR/words_test'
-#     train_dataset = IAMDataset(train_root_dir)
-#     test_dataset = IAMDataset(test_root_dir)
-#     """
-#     pass
+class IAMDataset(BaseDataset):
+    """
+    train_root_dir = '/home/mujahid/PycharmProjects/ssl_wordspotting/datasets/IEHR/words_training'
+    test_root_dir = '/home/mujahid/PycharmProjects/ssl_wordspotting/datasets/IEHR/words_test'
+    train_dataset = IAMDataset(train_root_dir)
+    test_dataset = IAMDataset(test_root_dir)
+    """
+    pass
 
 
 class IAMDataset2(Dataset):
@@ -81,7 +81,7 @@ class IAMDataset2(Dataset):
                 id = word.get('id')
                 idx = image_names_0.index(id)
                 samples.append((filtered_image_paths[idx], text))
-                print(samples)
+
         return samples
 
     def create_xml_paths(self):
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     lms = IAMDataset2(ttype='test').create_xml_paths()
     ims = IAMDataset2(ttype='test').create_dir_paths()
     sms = IAMDataset2(ttype='test').create_samples()
-    print(len(sms))
+    print(sms[:10])
 
     # print(lms[:10])
     # print(ims[:20])
